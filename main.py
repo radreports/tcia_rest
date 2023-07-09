5eeb60f00a2d6241c2fc23360ec6d7c55c2e94af
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2023-07-09 11:47:59 +0530

    HaN

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import pandas as pd
 import requests
 
 nbia.getToken()
+
+data = nbia.getSeries(collection = "Head-Neck-PET-CT", 
+                      api_url = "restricted")
+
+print(len(data), 'Series returned')
+
+df = nbia.downloadSeries(data, number = 3, api_url = "restricted", format = "df")
+display(df)
